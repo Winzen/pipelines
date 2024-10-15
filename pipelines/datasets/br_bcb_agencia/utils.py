b766f05301ff932e1af84c41557a86f50cf01bb4
Luiz Eduardo <luiz.sinx@gmail.com>
2024-10-15 13:08:14 -0300

    Update utils.py

--- pipelines/datasets/br_bcb_agencia/utils.py
+++ pipelines/datasets/br_bcb_agencia/utils.py
@@ -73,7 +73,7 @@ def download_and_unzip(url, extract_to):
         list: unziped files in a given folder
     """
 
-    test = 0 
+    test = 1 
     os.system(f"mkdir -p {extract_to}")
 
     http_response = urlopen(url)
